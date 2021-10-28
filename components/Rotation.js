a9478f84c9be9a266be7e536edb694ff8b0ba167
akcoder1050 <76036360+akcoder1050@users.noreply.github.com>
2021-10-28 15:00:31 +0530

    Add files via upload

---
+++ components/Rotation.js
@@ -0,0 +1,67 @@
+//Terrain Rotation
+AFRAME.registerComponent("terrain-rotation-reader", {
+  schema: {
+    speedOfRotation: { type: "number", default: 0 }    
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        if (this.data.speedOfRotation < 0.1) {
+          this.data.speedOfRotation += 0.01;
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (this.data.speedOfRotation > -0.1) {
+          this.data.speedOfRotation -= 0.01;
+        }
+      }
+    });
+  },
+  tick: function () {
+    var mapRotation = this.el.getAttribute("rotation");
+
+    mapRotation.y += this.data.speedOfRotation;
+
+    this.el.setAttribute("rotation", {
+      x: mapRotation.x,
+      y: mapRotation.y,
+      z: mapRotation.z
+    });
+  }
+});
+
+//Terrain Rotation
+AFRAME.registerComponent("plane-rotation-reader", {
+  schema: {
+    speedOfRotation: { type: "number", default: 0 },
+    speedOfAscent: { type: "number", default: 0 }  
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        if (this.data.speedOfRotation.x < 10) {
+          this.data.speedOfRotation.x += 0.5;
+          this.el.setAttribute("rotation", this.data.speedOfRotation);
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (this.data.speedOfRotation.x > -10) {
+          this.data.speedOfRotation.x -= 0.5;
+          this.el.setAttribute("rotation", this.data.speedOfRotation);
+        }
+      }
+      if (e.key === "ArrowUp") {
+        if (this.data.speedOfAscent.y < 10) {
+          this.data.speedOfAscent.y += 0.5;
+          this.el.setAttribute("positon", this.data.speedOfRotation);
+        }
+      }
+      if (e.key === "ArrowDown") {
+        if (this.data.speedOfAscent.y < -10) {
+          this.data.speedOfAscent.y -= 0.5;
+          this.el.setAttribute("positon", this.data.speedOfRotation);
+        }
+      }
+    });
+  }
+});
